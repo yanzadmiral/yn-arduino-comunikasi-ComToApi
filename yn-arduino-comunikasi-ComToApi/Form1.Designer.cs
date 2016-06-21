b64f450249eefa5bf0d6f31966e92e922d8d04db
Yayan Cahyani <yayanchy@gmail.com>
2016-06-21 07:38:41 +0700

    tambah

---
+++ yn-arduino-comunikasi-ComToApi/Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace yn_arduino_comunikasi_ComToApi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.receiveText = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.sendBtn = new System.Windows.Forms.Button();
+            this.url = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.comPort = new System.Windows.Forms.ComboBox();
+            this.baudRate = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.groupBox3.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox3.Controls.Add(this.receiveText);
+            this.groupBox3.Location = new System.Drawing.Point(10, 114);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(463, 284);
+            this.groupBox3.TabIndex = 10;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Received data";
+            // 
+            // receiveText
+            // 
+            this.receiveText.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.receiveText.Location = new System.Drawing.Point(6, 19);
+            this.receiveText.Multiline = true;
+            this.receiveText.Name = "receiveText";
+            this.receiveText.ReadOnly = true;
+            this.receiveText.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.receiveText.Size = new System.Drawing.Size(451, 223);
+            this.receiveText.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.sendBtn);
+            this.groupBox1.Controls.Add(this.url);
+            this.groupBox1.Location = new System.Drawing.Point(218, 9);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(255, 74);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Url";
+            // 
+            // sendBtn
+            // 
+            this.sendBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.sendBtn.Location = new System.Drawing.Point(174, 43);
+            this.sendBtn.Name = "sendBtn";
+            this.sendBtn.Size = new System.Drawing.Size(75, 23);
+            this.sendBtn.TabIndex = 1;
+            this.sendBtn.Text = "klik";
+            this.sendBtn.UseVisualStyleBackColor = true;
+            this.sendBtn.Click += new System.EventHandler(this.sendBtn_Click_1);
+            // 
+            // url
+            // 
+            this.url.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.url.Location = new System.Drawing.Point(7, 17);
+            this.url.Name = "url";
+            this.url.Size = new System.Drawing.Size(242, 20);
+            this.url.TabIndex = 0;
+            this.url.Text = "http://coba-coba.dev:81/skrispi-realtimedata/add-api.php";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.comPort);
+            this.groupBox2.Controls.Add(this.baudRate);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.btnConnect);
+            this.groupBox2.Location = new System.Drawing.Point(10, 8);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(201, 99);
+            this.groupBox2.TabIndex = 8;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Connection Manager";
+            // 
+            // comPort
+            // 
+            this.comPort.FormattingEnabled = true;
+            this.comPort.Location = new System.Drawing.Point(71, 18);
+            this.comPort.Name = "comPort";
+            this.comPort.Size = new System.Drawing.Size(121, 21);
+            this.comPort.TabIndex = 6;
+            // 
+            // baudRate
+            // 
+            this.baudRate.FormattingEnabled = true;
+            this.baudRate.Location = new System.Drawing.Point(71, 43);
+            this.baudRate.Name = "baudRate";
+            this.baudRate.Size = new System.Drawing.Size(121, 21);
+            this.baudRate.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Baud rate: ";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "COM:";
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(9, 72);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(183, 21);
+            this.btnConnect.TabIndex = 1;
+            this.btnConnect.Text = "Connect!";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(483, 403);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.groupBox2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed_1);
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.TextBox receiveText;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button sendBtn;
+        private System.Windows.Forms.TextBox url;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ComboBox comPort;
+        private System.Windows.Forms.ComboBox baudRate;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnConnect;
+    }
+}
+
---
+++ yn-arduino-comunikasi-ComToApi/Form1.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Collections.Specialized;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Threading;
+using System.Net;
+
+namespace yn_arduino_comunikasi_ComToApi
+{
+    public partial class Form1 : Form
+    {
+        public string kl = "http://coba-coba.dev:81/skrispi-realtimedata/rest-api.php";
+        public static System.IO.Ports.SerialPort port;
+        delegate void SetTextCallback(string text);
+
+        // This BackgroundWorker is used to demonstrate the 
+        // preferred way of performing asynchronous operations.
+        private BackgroundWorker hardWorker;
+
+        private Thread readThread = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            hardWorker = new BackgroundWorker();
+           
+        }
+
+        private void SetText(string text)
+        {
+            // InvokeRequired required compares the thread ID of the
+            // calling thread to the thread ID of the creating thread.
+            // If these threads are different, it returns true.
+            if (this.receiveText.InvokeRequired)
+            {
+                SetTextCallback d = new SetTextCallback(SetText);
+                this.Invoke(d, new object[] { text });
+            }
+            else
+            {
+
+                string a = Convert.ToString(text);
+                string[] b = a.Split('#');
+                string c = Convert.ToString(b.Count());
+
+                if (b.Count() == 5)
+                {
+                    this.receiveText.Text = text;
+                    atur2(b);
+                }
+            }
+        }
+        public void atur2(string[] data)
+        {
+            WebClient objWebClient = new WebClient();
+
+            NameValueCollection objNameValueCollection = new NameValueCollection();
+            objNameValueCollection.Add("suhu", data[0]);
+            objNameValueCollection.Add("lpg", data[1]);
+            objNameValueCollection.Add("co", data[2]);
+            objNameValueCollection.Add("api", data[3]);
+            objNameValueCollection.Add("hasil", data[4]);
+
+            byte[] bytes = objWebClient.UploadValues(kl, "POST", objNameValueCollection);
+            //MessageBox.Show(Encoding.ASCII.GetString(bytes));
+        }
+        public void Read()
+        {
+            while (port.IsOpen)
+            {
+                try
+                {
+                    if (port.BytesToRead > 0)
+                    {
+                        string message = port.ReadLine();
+                        this.SetText(message);
+                    }
+                }
+                catch (TimeoutException) { }
+            }
+        }
+
+
+        private void btnConnect_Click_1(object sender, EventArgs e)
+        {
+            System.ComponentModel.IContainer components =
+            new System.ComponentModel.Container();
+            port = new System.IO.Ports.SerialPort(components);
+            port.PortName = comPort.SelectedItem.ToString();
+            port.BaudRate = Int32.Parse(baudRate.SelectedItem.ToString());
+            port.DtrEnable = true;
+            port.ReadTimeout = 5000;
+            port.WriteTimeout = 500;
+            port.Open();
+
+            readThread = new Thread(new ThreadStart(this.Read));
+            readThread.Start();
+            this.hardWorker.RunWorkerAsync();
+
+            btnConnect.Text = "<Connected>";
+
+            btnConnect.Enabled = false;
+            comPort.Enabled = false;
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            foreach (string s in SerialPort.GetPortNames())
+            {
+                comPort.Items.Add(s);
+            }
+            if (comPort.Items.Count > 0)
+                comPort.SelectedIndex = comPort.Items.Count - 1;
+            else
+                comPort.SelectedIndex = 0;
+
+            baudRate.Items.Add("2400");
+            baudRate.Items.Add("4800");
+            baudRate.Items.Add("9600");
+            baudRate.Items.Add("14400");
+            baudRate.Items.Add("19200");
+            baudRate.Items.Add("28800");
+            baudRate.Items.Add("38400");
+            baudRate.Items.Add("57600");
+            baudRate.Items.Add("115200");
+
+            baudRate.SelectedIndex = 2;
+        }
+
+
+        private void sendBtn_Click_1(object sender, EventArgs e)
+        {
+            kl = url.Text;
+        }
+
+        private void Form1_FormClosed_1(object sender, FormClosedEventArgs e)
+        {
+            try
+            {
+                if (!(readThread == null))
+                    readThread.Abort();
+            }
+            catch (NullReferenceException)
+            {
+            }
+
+            try
+            {
+                port.Close();
+            }
+            catch (NullReferenceException)
+            {
+            }
+        }
+    }
+}
